251b959b62c31bf94b18d2f6e8722e88e95576a0
Swaiidan <ibra0529@easv.dk>
2021-06-06 19:43:45 +0200

    added the prepared Statement method, but we still need to import the variables from the App_UserRegister

--- TheStoryLineTool/src/com/DB/Test/DBTest.java
+++ TheStoryLineTool/src/com/DB/Test/DBTest.java
@@ -1,9 +1,16 @@
 package com.DB.Test;
 
+import com.Application.controller.user.User.App_UserRegister;
 import com.DB.util.ConnectionUtil;
+import com.Domain.user.Dom_User;
+import com.Function.user.Func_User;
+import com.GUI.User.GUI_UserRegister;
+import javafx.scene.control.TextField;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 import static com.DB.util.CRUD_Util.*;
 
@@ -24,20 +31,50 @@ public class DBTest {
     }
 
     @Test
-    public void testInsert() {
-        try {
-            String sql = "INSERT INTO  schema_TestTable.tbl_TestTable(column_1, column_2, column_3) values (?,?,?) ";
-            update(sql, 1, "Nordea","debt");
-            update(sql, 2, "DanskBank","card");
-            update(sql, 3, "SydDenmarkBank","card");
-            System.out.println("Insert finished");
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void testInsert  () {
+String userName;
+String password;
+
+        // import the dom_user object, the username and password from App_UserRegister because
+        // there it has been converted from TextField to String
+        App_UserRegister RegisterObject= new App_UserRegister();
+      dom_user(userName, password);
+
+            try{
+                // (1) load the driver into memory
+
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+                // (2) establish Connection
+                Connection con= DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=StoryLineTool","sa","123456");
+
+
+                // (3) create the statement
+                PreparedStatement ps = con.prepareStatement("INSERT into tbl_User VALUES (?,?)");
+
+
+                // (4) Set values
+                ps.setString(1, String.valueOf(userName));
+                ps.setString(2,String.valueOf(password));
+
+
+
+                // 4.1 execute the Update
+                ps.executeUpdate();
+
+                // (5) close the connection
+                con.close();
+
+            }catch(Exception e){
+                System.out.println("register finish");
+                e.printStackTrace();
+            }
+
         }
 
-    }
 
-    @Test
+
+        @Test
     public void testUpdate() {
         try {
             String sql = "UPDATE schema_TestTable.tbl_TestTable SET column_2 = ? where column_1 = ?";
